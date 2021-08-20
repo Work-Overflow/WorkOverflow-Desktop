e5683cf1bc64f2dea4f76e07726eb3f77c79a936
Dwijottam <dwijottamdutta@gmail.com>
2021-08-20 18:02:41 +0530

    Repairing Security Issues with data

--- Js/workoverflow.js
+++ Js/workoverflow.js
@@ -9,9 +9,8 @@ const deleteAllBtn = document.querySelector(".footer button");
 let dark_mode_btn = document.querySelector('.dark_mode_btn');
 let music_mode_btn = document.querySelector('.music_mode_btn');
 let audio = new Audio('music.mp3');
-let User = localStorage.getItem("User Name");
+let UserVer = localStorage.getItem("User Name");
 let Todo = localStorage.getItem("Todo Name");
-
 
 if (Todo == null || Todo == ""){
   todoname.innerHTML = "Untitled Todo";
@@ -19,6 +18,18 @@ if (Todo == null || Todo == ""){
 else{
   todoname.innerHTML = Todo;
   document.title = "Work Overflow | "+Todo;
+}
+
+if (UserVer == null || UserVer == ""){
+  userproduct = prompt("Enter your name:");
+  localStorage.setItem("User Name", userproduct);
+  // firebase.initializeApp(firebaseConfig);
+  const database = firebase.database();
+  let data={
+    User: userproduct
+  }
+  let ref = database.ref("Users_Data/" + userproduct);
+	ref.set(data);
 }
 
 
